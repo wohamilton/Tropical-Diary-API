24f7b48043c7db91ae0cad8a7438bc82299197a5
bill <wohamilton@gmail.com>
2015-11-15 15:54:36 +0800

    working

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
-var loopback = require('loopback');
+var bodyParser = require('body-parser');
 var boot = require('loopback-boot');
-
+var loopback = require('loopback');
+var path = require('path');
 var app = module.exports = loopback();
+
+app.middleware('initial', bodyParser.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'jade'); // LoopBack comes with EJS out-of-box
+app.set('json spaces', 2); // format json responses for easier viewing
+
+// must be set to serve views properly when starting the app via `slc run` from
+// the project root
+app.set('views', path.resolve(__dirname, 'views'));
 
 app.start = function() {
   // start the web server
